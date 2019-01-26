715008ac5917fe1a592eb2f392289b77595e8ccb
xiaoyiyi <xiaoyishikongde@163.com>
2019-01-26 16:42:04 +0800

    加paper

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var result = `/*
 *首先准备一些样式
 */
 
-*{
+{
     transition : all 1s;
 }
 html{
@@ -17,7 +17,7 @@ html{
     font-size:16px;
 }
 #code{
-    border:1px solid #DDA;
+    border:1px solid #AAA;
     padding:16px;
 }
 
@@ -32,53 +32,99 @@ html{
 .token.function{
     color: #DD4A68;
 }
-
-/* 加点3D效果 */
+/* 加一个呼吸效果 */
 #code{
-    transform:rotate(360deg);
+animation: breath 0.5s infinite alternate-reverse;
 }
+
 /* 不玩了，我来介绍一下我自己吧 
+ * 现在正是开始
  * 我需要一张白纸 */
 
+#code-wrapper{
+    width: 50%; left: 0; position: fixed; 
+    height: 100%;}
+#paper > .content{
+    display: block;
+    background:white;
+}
+`
+var result2 = `
+/*
+*接下来把Markdown变成HTML marked.js     
+*接下来给HTML加样式
+*这就是我的会动的简历
+*谢谢观看！
+*/
 `
 
-var n = 0
-var id = setInterval(() => {
-    n += 1
-    code.innerHTML = result.substring(0, n)
-    code.innerHTML =
-        Prism.highlight(code.innerHTML, Prism.languages.css);
-    styleTag.innerHTML = result.substring(0, n)
-    if (n >= result.length) {
-        window.clearInterval(id)
-        fn2()
-        fn3(result)
-    }
-}, 10)
+var md = `
+#自我介绍
 
-function fn2() {
+我叫肖伊
+武汉东湖学院计算机科学与技术专业2019届毕业生
+希望应聘前端开发岗位
+
+#技能介绍
+熟悉HTML5+CSS3，Javascript等
+
+#项目介绍
+1.canvas画板
+2.动态简历
+3.轮播demo
+
+#联系方式
+Phone：15972218784
+Wechat:1597718784
+QQ:734732741
+E-mail:xiaoyishikongde@163.com
+`
+// WriteCode(result)异步代码（不等结果直接进行下一步） 需要回调
+WriteCode('', result, () => {
+    createPaper(() => {
+        WriteCode(result, result2,() => {
+            writeMarkdown(md)
+        })
+    })
+})
+function WriteCode(prefix, code, fn) {
+    let domCode = document.querySelector('#code')
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+        styleTag.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollTop = domCode.scrollHeight
+        if (n >= code.length) {
+            window.clearInterval(id)
+            fn && fn.call()
+        }
+    }, 20)
+}
+
+
+function createPaper(fn) {
     var paper = document.createElement('div')
     paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.className = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
+    fn && fn.call()
 }
 
-function fn3(preResult) {
-    var result = `
-#paper{
-width:100px;height:100px;
-background:#DDA;
-}
-    `
-    var n = 0
-    var id = setInterval(() => {
+function writeMarkdown(markdown, fn) {
+    let domPaper = document.querySelector('#paper> .content')
+    let n = 0
+    let id = setInterval(() => {
         n += 1
-        code.innerHTML = preResult+result.substring(0, n)
-        code.innerHTML =
-        Prism.highlight(code.innerHTML, Prism.languages.css)
-        styleTag.innerHTML = preResult+result.substring(0, n)
-        if(n >= result.length){
+        domPaper.innerHTML = markdown.substring(0, n)
+        domPaper.scrollTop = domPaper.scrollHeight
+        if (n >= markdown.length) {
             window.clearInterval(id)
+            fn && fn.call()
         }
-    }, 50)
+    }, 20)
+
 }
 
